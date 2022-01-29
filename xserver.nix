14509f3a9be79fc16755d7633052a8056c971bb5
Vanilla <osu_Vanilla@126.com>
2022-01-29 15:16:31 +0800

    style[xserver]: Using inputs.Vanilla enable GNOME module.

--- xserver.nix
+++ xserver.nix
@@ -3,11 +3,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   imports = [
+    "${inputs.Vanilla}/enableGNOME.nix"
     "${inputs.Vanilla}/minimalGNOME.nix"
   ];
 }
