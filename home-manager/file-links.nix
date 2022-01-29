9f8173c4a98f90d586ed96c8d101bb60511af203
Vanilla <osu_Vanilla@126.com>
2022-01-29 17:29:03 +0800

    perf[home-manager]: Persist the .config/monitors.xml files.

--- home-manager/file-links.nix
+++ home-manager/file-links.nix
@@ -7,4 +7,8 @@
   home.file."Projects".source =
     config.lib.file.mkOutOfStoreSymlink
       "/persistent/Projects";
+
+  home.file.".config/monitors.xml".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot/config/monitors.xml";
 }
