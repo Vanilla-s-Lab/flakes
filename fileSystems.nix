4db3ed469575cdae137a972571b8416ddac23556
Vanilla <osu_Vanilla@126.com>
2022-01-29 11:33:53 +0800

    style[disks]: Remove resident line break from /nix and /persistent.

--- fileSystems.nix
+++ fileSystems.nix
@@ -5,17 +5,15 @@
     "/boot" = { device = "/dev/disk/by-uuid/2F71-8FE8"; fsType = "vfat"; };
   };
 
-  fileSystems."/nix" =
-    {
-      device = "/dev/disk/by-uuid/49a55786-a043-400f-a6cb-3444ddda7975";
-      fsType = "btrfs";
-      options = [ "subvol=nix" "noatime" "compress-force=zstd" ];
-    };
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/49a55786-a043-400f-a6cb-3444ddda7975";
+    fsType = "btrfs";
+    options = [ "subvol=nix" "noatime" "compress-force=zstd" ];
+  };
 
-  fileSystems."/persistent" =
-    {
-      device = "/dev/disk/by-uuid/49a55786-a043-400f-a6cb-3444ddda7975";
-      fsType = "btrfs";
-      options = [ "subvol=persistent" "noatime" "compress-force=zstd" ];
-    };
+  fileSystems."/persistent" = {
+    device = "/dev/disk/by-uuid/49a55786-a043-400f-a6cb-3444ddda7975";
+    fsType = "btrfs";
+    options = [ "subvol=persistent" "noatime" "compress-force=zstd" ];
+  };
 }
