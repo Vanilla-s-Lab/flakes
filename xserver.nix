751109356b6bd00c2fae71c250dfc0303af8ec6c
Vanilla <osu_Vanilla@126.com>
2022-01-29 15:14:05 +0800

    style[xserver]: Using inputs.Vanilla minimal GNOME module.

--- xserver.nix
+++ xserver.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs, ... }:
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -7,7 +7,7 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # NixOS/nixpkgs - nixos/modules/services/x11/desktop-managers/gnome.nix
-  services.gnome.core-utilities.enable = false;
-  services.gnome.core-developer-tools.enable = false;
+  imports = [
+    "${inputs.Vanilla}/minimalGNOME.nix"
+  ];
 }
