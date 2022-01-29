a16b826b9e0f68b8e755d538ef037d3d37ca4d40
Vanilla <osu_Vanilla@126.com>
2022-01-29 21:28:17 +0800

    style[home-manager]: Reformat home.nix, no wrapping each file.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,13 +1,17 @@
 { pkgs, ... }:
 {
-  imports = [ ./programs/chromium.nix ]
-    ++ [ ./dconf.nix ./file-links.nix ]
-    ++ [ ./programs/alacritty.nix ]
-    ++ [ ./fonts.nix ]
-    ++ [ ./programs/qv2ray.nix ]
-    ++ [ ./programs/git.nix ]
-    ++ [ ./gnome.nix ]
-    ++ [ ./programs/albert.nix ];
+  imports = [
+    ./programs/chromium.nix
+    ./programs/alacritty.nix
+    ./programs/qv2ray.nix
+    ./programs/git.nix
+    ./programs/albert.nix
+
+    ./dconf.nix
+    ./file-links.nix
+    ./fonts.nix
+    ./gnome.nix
+  ];
 
   home.packages = [ pkgs.nixpkgs-fmt ];
   home.stateVersion = "21.11";
