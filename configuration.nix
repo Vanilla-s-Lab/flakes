97d3965ffb9ccacc712024d376910b735f6d7cde
Vanilla <osu_Vanilla@126.com>
2022-01-29 11:38:50 +0800

    rename[hardware]: -> ./hardware/fileSystems.nix.

--- configuration.nix
+++ configuration.nix
@@ -9,8 +9,7 @@
     [
       # Include the results of the hardware scan.
       ./hardware/configuration.nix
-
-      ./fileSystems.nix
+      ./hardware/fileSystems.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
