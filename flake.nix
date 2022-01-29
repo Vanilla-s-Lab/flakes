bb0f3d39d6f49279d12b71668f7040dcfefd4ee6
Vanilla <osu_Vanilla@126.com>
2022-01-29 15:33:40 +0800

    feat[home-manager]: Init home-manager nixos module in flake.nix.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,15 @@
     nixos.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     Vanilla.url = "github:Vanilla-s-Lab/Vanilla";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs = { self, ... }@inputs: with inputs;
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: {
       packages.nixosConfigurations."NixOS-RoT" = nixos.lib.nixosSystem rec {
         inherit system; specialArgs = { inherit inputs; };
-        modules = [ ./configuration.nix ];
+        modules = [ ./configuration.nix home-manager.nixosModules.home-manager ]
+          ++ [{ home-manager.users."vanilla" = import ./home-manager/home.nix; }];
       };
     });
 }
