bcb786fe8559fc94a47662ad1b26754c9c37a68f
Vanilla <osu_Vanilla@126.com>
2022-01-29 21:58:02 +0800

    feat[fonts]: Using inputs.Vanilla, import Chinese fonts fix.

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, inputs, ... }:
 {
   # https://github.com/nix-community/home-manager/issues/1118
   fonts.fontconfig.enable = lib.mkForce true;
@@ -8,4 +8,6 @@
     pkgs.nur.repos.vanilla.Win10_LTSC_2021_fonts
     pkgs.nur.repos.vanilla.apple-fonts.SF-Compact
   ];
+
+  imports = [ "${inputs.Vanilla}/fixChinese.nix" ];
 }
