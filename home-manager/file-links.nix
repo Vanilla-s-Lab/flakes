1996925b7c3503378bb562035795fc481b4abba3
Vanilla <osu_Vanilla@126.com>
2022-01-29 17:27:39 +0800

    perf[home-manager]: Link home Projects to /persistent/Projects.

--- home-manager/file-links.nix
+++ home-manager/file-links.nix
@@ -3,4 +3,8 @@
   home.file.".cache/nix".source =
     config.lib.file.mkOutOfStoreSymlink
       "/persistent/dot/cache/nix";
+
+  home.file."Projects".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/Projects";
 }
