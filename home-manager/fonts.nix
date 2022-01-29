57b98c6b7c5e45375a4222d6d3e6b3523d921691
Vanilla <osu_Vanilla@126.com>
2022-01-29 21:50:21 +0800

    feat[fonts]: Add apple-fonts - SF-Compact, use as display font.

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -6,5 +6,6 @@
   home.packages = [
     (pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
     pkgs.nur.repos.vanilla.Win10_LTSC_2021_fonts
+    pkgs.nur.repos.vanilla.apple-fonts.SF-Compact
   ];
 }
