a1dc182e240f755691aff9c6831e94645cc97b43
Vanilla <osu_Vanilla@126.com>
2022-01-29 19:27:07 +0800

    perf[dconf]: Enable useful extensions, list are bundled.

--- home-manager/dconf.nix
+++ home-manager/dconf.nix
@@ -3,4 +3,17 @@
   imports = [
     "${inputs.Vanilla}/gnomeNoWelcome.nix"
   ];
+
+  dconf.settings = {
+    "org/gnome/shell".enabled-extensions = [
+      "apps-menu@gnome-shell-extensions.gcampax.github.com"
+      "launch-new-instance@gnome-shell-extensions.gcampax.github.com"
+      "places-menu@gnome-shell-extensions.gcampax.github.com"
+      "drive-menu@gnome-shell-extensions.gcampax.github.com"
+      "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com"
+      "user-theme@gnome-shell-extensions.gcampax.github.com"
+      "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+      "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+    ];
+  };
 }
