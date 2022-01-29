6c3f2b66b70012c04016670d97dd5dd6711bf845
Vanilla <osu_Vanilla@126.com>
2022-01-29 14:58:27 +0800

    feat[hardware]: Enable NVIDIA driver, also enable modesetting.

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     # Include the results of the hardware scan.
     ./hardware/configuration.nix
     ./hardware/fileSystems.nix
+    ./hardware/nvidia.nix
 
     ./users-immutable.nix
 
---
+++ hardware/nvidia.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  nixpkgs.config.allowUnfree = true;
+
+  hardware.nvidia.modesetting.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia.prime.sync.enable = true;
+  hardware.nvidia.prime.intelBusId = "PCI:0:2:0";
+  hardware.nvidia.prime.nvidiaBusId = "PCI:1:0:0";
+}
