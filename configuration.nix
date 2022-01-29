65c341dc76e6fb8130c054a6fef29358f2725d68
Vanilla <osu_Vanilla@126.com>
2022-01-29 12:02:55 +0800

    style[xserver]: Separate xserver config -> xserver.nix.

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,8 @@
 
       ./networking/hostName-domain.nix
       ./networking/networkmanager.nix
+
+      ./xserver.nix
     ];
 
   # Set your time zone.
@@ -33,15 +35,6 @@
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
   # };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
