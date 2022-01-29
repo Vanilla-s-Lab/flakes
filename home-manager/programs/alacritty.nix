512cc1003f8008d9ce8028c5806486b02df78d7e
Vanilla <osu_Vanilla@126.com>
2022-01-29 17:06:31 +0800

    feat[programs]: Let alacritty terminal use Hack Nerd Font.

--- home-manager/programs/alacritty.nix
+++ home-manager/programs/alacritty.nix
@@ -6,5 +6,12 @@
       url = "https://raw.githubusercontent.com/rose-pine/alacritty/main/rose-pine-dawn.yml";
       sha256 = "sha256-/1y/sYIugR27QgbM2rG4WganTPbdqbAgnbrjkDXIgUg=";
     });
+
+    font = {
+      normal = { family = "Hack Nerd Font"; style = "Regular"; };
+      bold = { family = "Hack Nerd Font"; style = "Bold"; };
+      italic = { family = "Hack Nerd Font"; style = "Italic"; };
+      size = 11.0;
+    };
   };
 }
