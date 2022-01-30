678e37f402584527add72e02e140f8a0afe0d19e
Vanilla <osu_Vanilla@126.com>
2022-01-30 23:25:49 +0800

    fix[gnome]: Fix type of window size in system-monitor.

--- home-manager/gnome/system-monitor.nix
+++ home-manager/gnome/system-monitor.nix
@@ -1,10 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = [ pkgs.gnome.gnome-system-monitor ];
   dconf.settings."org/gnome/gnome-system-monitor" = {
     current-tab = "resources";
-    window-state = "(700, 526, 50, 50)";
-
-    maximized = false;
+    window-state = lib.hm.gvariant.mkTuple [ 700 526 50 50 ];
   };
 }
