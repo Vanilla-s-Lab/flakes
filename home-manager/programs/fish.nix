9595b3242201d787dfef051a51198dc114bec8f0
Vanilla <osu_Vanilla@126.com>
2022-01-30 23:03:12 +0800

    perf[fish]: Persist fish command history folder.

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
   programs.fish.enable = true;
   home.packages = [
@@ -8,4 +8,8 @@
   home.activation."fishPlugins.git" =
     lib.hm.dag.entryAfter [ "dconfSettings" ]
       "fish -c \"__git.init\" && true";
+
+  home.file.".local/share/fish/fish_history".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot/local/share/fish/fish_history";
 }
