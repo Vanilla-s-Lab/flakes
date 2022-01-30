070873d19eb644727c2d30a6f482a2f99b6c8599
Vanilla <osu_Vanilla@126.com>
2022-01-30 12:05:41 +0800

    perf[gnome]: Adjusting dconf for gnome tweak apps.

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -1,4 +1,25 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.gnome.gnome-tweaks ];
+  home.packages = [
+    pkgs.gnome.gnome-tweaks
+
+    pkgs.layan-gtk-theme
+    pkgs.breeze-gtk
+    pkgs.tela-icon-theme
+    pkgs.flat-remix-gnome
+  ];
+
+  dconf.settings = {
+    # Extensions Has Moved - Continue
+    "org/gnome/tweaks".show-extensions-notice = false;
+
+    # Genernal - Over-Amplification - ON
+    "org/gnome/desktop/sound".allow-volume-above-100-percent = true;
+
+    # Appearance - Applications | Cursor | Icons | Shell
+    "org/gnome/desktop/interface".gtk-theme = "Layan-light-solid";
+    "org/gnome/desktop/interface".cursor-theme = "breeze_cursors";
+    "org/gnome/desktop/interface".icon-theme = "Tela-pink";
+    "org/gnome/shell/extensions/user-theme".name = "Flat-Remix-Blue-Light";
+  };
 }
