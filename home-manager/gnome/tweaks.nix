bb71b27980ba4bd36f07b4ff556c3be2158e2f00
Vanilla <osu_Vanilla@126.com>
2022-01-30 22:34:20 +0800

    perf[gnome]: Setting up desktop and lock screen bg.

--- home-manager/gnome/tweaks.nix
+++ home-manager/gnome/tweaks.nix
@@ -16,10 +16,14 @@
     # Genernal - Over-Amplification - ON
     "org/gnome/desktop/sound".allow-volume-above-100-percent = true;
 
-    # Appearance - Applications | Cursor | Icons | Shell
+    # Appearance - Themes - Applications | Cursor | Icons | Shell
     "org/gnome/desktop/interface".gtk-theme = "Layan-light-solid";
     "org/gnome/desktop/interface".cursor-theme = "breeze_cursors";
     "org/gnome/desktop/interface".icon-theme = "Tela-pink";
     "org/gnome/shell/extensions/user-theme".name = "Flat-Remix-Blue-Light";
+
+    # Appearance - [ Background | Lock Screen ] - Image
+    "org/gnome/desktop/background".picture-uri = "file:///home/vanilla/Pictures/bg.jpg";
+    "org/gnome/desktop/screensaver"picture-uri = "file:///home/vanilla/Pictures/bg.jpg";
   };
 }
