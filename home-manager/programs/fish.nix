4ee373f9bcdd29783889297ffb7d6804d53465d9
Vanilla <osu_Vanilla@126.com>
2022-01-30 23:48:46 +0800

    feat[fish]: Enable pisces and bang-bang.

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -3,7 +3,8 @@
   programs.fish.enable = true;
   home.packages = [
     pkgs.nur.repos.linyinfeng.fishPlugins.git
-  ];
+    pkgs.nur.repos.linyinfeng.fishPlugins.bang-bang
+  ] ++ [ pkgs.fishPlugins.pisces ];
 
   home.activation."fishPlugins.git" =
     lib.hm.dag.entryAfter [ "dconfSettings" ]
