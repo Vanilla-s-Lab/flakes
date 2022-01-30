a6c6265030dad09678cec183eea102845fed644d
Vanilla <osu_Vanilla@126.com>
2022-01-30 11:32:36 +0800

    fix[git]: Update pinentry tty for git commit.

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -3,4 +3,7 @@
   programs.git.enable = true;
   programs.git.userName = "Vanilla";
   programs.git.userEmail = "osu_Vanilla@126.com";
+
+  programs.git.signing.key = "3750028ED04FA42E";
+  programs.git.signing.signByDefault = true;
 }
--- home-manager/services/gpg-agent.nix
+++ home-manager/services/gpg-agent.nix
@@ -2,5 +2,8 @@
 {
   services.gpg-agent.enable = true;
   services.gpg-agent.enableSshSupport = true;
+
   services.gpg-agent.pinentryFlavor = "curses";
+  programs.fish.interactiveShellInit =
+    "gpg-connect-agent updatestartuptty /bye > /dev/null";
 }
