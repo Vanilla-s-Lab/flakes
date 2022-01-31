200e2ed8fd211fe6003a198b1895dd5ead1ff7e3
Vanilla <osu_Vanilla@126.com>
2022-01-31 21:13:26 +0800

    perf[gnome]: Finish most of Settings items, remain Data & Time.

--- home-manager/gnome/settings.nix
+++ home-manager/gnome/settings.nix
@@ -3,9 +3,22 @@
   dconf.settings = {
     # Settings - Search - OFF
     "org/gnome/desktop/search-providers".disable-external = true;
+
     # Settings - Privacy - Camera - OFF
     "org/gnome/desktop/privacy".disable-camera = true;
     # Settings - Privacy - Microphone - OFF
     "org/gnome/desktop/privacy".disable-microphone = true;
+
+    # Settings - Displays - Night Light - ON
+    "settings-daemon/plugins/color".night-light-enabled = true;
+
+    # Settings - Mouse & Touchpad - Tap to click - ON
+    "org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
+
+    # Settings - Removable Media - Never prompt or start programs on media insertion
+    "org/gnome/desktop/media-handling".autorun-never = true;
+
+    # Settings - Accessibility - Always Show Accessibility Menu - ON
+    "org/gnome/desktop/a11y".always-show-universal-access-status = true;
   };
 }
