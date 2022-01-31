cf33ebff06026527d0c66a5a841bffe7dc8e40a5
Vanilla <osu_Vanilla@126.com>
2022-01-31 12:06:24 +0800

    perf[gnome]: Add and config the extension Dash to Dock.

--- home-manager/gnome/extenal-ext.nix
+++ home-manager/gnome/extenal-ext.nix
@@ -5,6 +5,7 @@
     pkgs.gnomeExtensions.bluetooth-quick-connect
     pkgs.gnomeExtensions.blur-my-shell
     pkgs.gnomeExtensions.bring-out-submenu-of-power-offlogout-button
+    pkgs.gnomeExtensions.dash-to-dock
 
     pkgs.gnomeExtensions.unite
     pkgs.gnomeExtensions.runcat
@@ -13,7 +14,6 @@
     pkgs.gnomeExtensions.simple-net-speed
     pkgs.gnomeExtensions.proxy-switcher
     pkgs.gnomeExtensions.screenshot-tool
-    pkgs.gnomeExtensions.dash-to-dock
     pkgs.gnomeExtensions.sound-output-device-chooser
     pkgs.gnomeExtensions.status-area-horizontal-spacing
     pkgs.gnomeExtensions.window-is-ready-remover
@@ -30,6 +30,7 @@
       "bluetooth-quick-connect@bjarosze.gmail.com"
       "blur-my-shell@aunetx"
       "BringOutSubmenuOfPowerOffLogoutButton@pratap.fastmail.fm"
+      "dash-to-dock@micxgx.gmail.com"
     ];
 
     # Button Appearance - Menu Button Appearance - Appearance - Icon and Text
@@ -49,5 +50,21 @@
     # Blur my shell - Blue - [ dash | panel | window list extension ] - OFF
     "org/gnome/shell/extensions/blur-my-shell" =
       { blur-dash = false; blur-panel = false; blur-window-list = false; };
+
+    # Dash to Dock - Position and size - Show on all monitors.
+    "org/gnome/shell/extensions/dash-to-dock".multi-monitor = true;
+    # Dash to Dock - Launchers - Isolate [ workspaces | monitors ].
+    "org/gnome/shell/extensions/dash-to-dock".isolate-workspaces = true;
+    "org/gnome/shell/extensions/dash-to-dock".isolate-monitors = true;
+    # Dash to Dock - Behavior - Click action - Minimize
+    "org/gnome/shell/extensions/dash-to-dock".click-action = "minimize";
+    # Dash to Dock - Behavior - Scroll action - Cycle through windows
+    "org/gnome/shell/extensions/dash-to-dock".scroll-action = "cycle-windows";
+    # Dash to Dock - Appearance - Customize the dash color - ON
+    "org/gnome/shell/extensions/dash-to-dock".custom-background-color = true;
+    # https://matkl.github.io/average-color/ + ~/Pictures/bg.jpg
+    "org/gnome/shell/extensions/dash-to-dock".background-color = "rgb(151,126,128)";
+    # Dash to Dock - Appearance - Customize opacity - Dynamic
+    "org/gnome/shell/extensions/dash-to-dock".transparency-mode = "DYNAMIC";
   };
 }
