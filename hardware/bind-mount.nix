c7c5fe04e84ecdc574aded353fcc601f09f13067
Vanilla <osu_Vanilla@126.com>
2022-02-01 02:49:56 +0800

    fix[disks]: Mount .local/share as tmpfs, fix permission issues.

--- hardware/bind-mount.nix
+++ hardware/bind-mount.nix
@@ -1,8 +1,17 @@
-{ ... }:
+{ config, ... }:
 {
   # https://github.com/fish-shell/fish-shell/issues/8627
   fileSystems."/home/vanilla/.local/share/fish" = {
     device = "/persistent/dot/local/share/fish";
     options = [ "bind" ];
+  };
+
+  users.users."vanilla".uid = 1000;
+  users.groups."users".gid = 100;
+
+  fileSystems."/home/vanilla/.local/share" = {
+    fsType = "tmpfs";
+    options = [ "defaults" "size=2G" ]
+      ++ [ "uid=1000" "gid=100" ];
   };
 }
