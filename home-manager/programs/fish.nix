6aa0d01760be3b8481f255bad5888476c3df34a1
Vanilla <osu_Vanilla@126.com>
2022-02-01 13:21:28 +0800

    perf[fish]: Enable Rose Pine Dawn theme.

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -9,4 +9,14 @@
   home.activation."fishPlugins.git" =
     lib.hm.dag.entryAfter [ "dconfSettings" ]
       "fish -c \"__git.init\" && true";
+
+  home.file.".config/fish/functions/rose_pine.fish".source =
+    pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/rose-pine/fish/main/rose_pine.fish";
+      hash = "sha256-RSC7d0x+m2FWtNyN8WmmHNcTn2gL6ZS5tRsc3KJB8Hc=";
+    };
+
+  programs.fish.interactiveShellInit = ''
+    rose_pine dawn > /dev/null
+  '';
 }
