a75ad6d20311359bb78186aabc110adcae8e7331
Vanilla <osu_Vanilla@126.com>
2022-02-02 20:11:51 +0800

    perf[programs]: Disable IPC for ASF.

--- home-manager/config/ArchiSteamFarm.nix
+++ home-manager/config/ArchiSteamFarm.nix
@@ -5,7 +5,7 @@
   ];
 
   home.file.".config/archisteamfarm/config/ASF.json".text =
-    builtins.toJSON { Statistics = false; UpdateChannel = 0; };
+    builtins.toJSON { IPC = false; Statistics = false; UpdateChannel = 0; };
 
   home.file.".config/archisteamfarm/config/Vanilla.json".source =
     config.lib.file.mkOutOfStoreSymlink
