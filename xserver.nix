7ac4bcca55d4435bfb24cd7c141ffae69d9aa706
Vanilla <osu_Vanilla@126.com>
2022-02-03 00:27:23 +0800

    style[xserver]: Remove inputs.Vanilla, add more comments.

--- xserver.nix
+++ xserver.nix
@@ -1,13 +1,19 @@
-{ inputs, ... }:
+{ ... }:
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  imports = [
-    "${inputs.Vanilla}/enableGNOME.nix"
-    "${inputs.Vanilla}/minimalGNOME.nix"
-  ];
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
+  # Disable most of GNOME bundled tools.
+  services.gnome = {
+    core-utilities.enable = false;
+    core-developer-tools.enable = false;
+  };
+
+  # https://github.com/NixOS/nixpkgs/pull/107850
   systemd.services."display-manager".preStart =
     "cp /persistent/dot/config/monitors.xml /run/gdm/.config/monitors.xml";
 }
