bd3dd8732dc70fd4e7979730216e46c1c707f387
Vanilla <osu_Vanilla@126.com>
2022-02-03 11:38:28 +0800

    feat[netowrking]: Enable 1089 and 8889 for Android.

--- networking.nix
+++ networking.nix
@@ -15,4 +15,8 @@
 
   # intel/ibt-17-16-1.sfi | rtl_nic/rtl8411-2.fw
   hardware.firmware = [ pkgs.linux-firmware ];
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 1089 8889 ];
+  networking.firewall.allowedUDPPorts = [ 1089 8889 ];
 }
