75a210be1a75ac7046c3dc256243ed1c8bea60a6
Vanilla <osu_Vanilla@126.com>
2022-02-03 10:20:44 +0800

    style[obs]: Split home.packages section.

--- home-manager/programs/obs-studio.nix
+++ home-manager/programs/obs-studio.nix
@@ -1,6 +1,9 @@
 { pkgs, config, ... }:
 {
-  home.packages = [ pkgs.obs-studio ];
+  home.packages = [
+    pkgs.obs-studio
+  ];
+
   home.file.".config/obs-studio".source =
     config.lib.file.mkOutOfStoreSymlink
       "/persistent/dot/config/obs-studio";
