ff37ac9c157355191e4a461b1fc7a6f8f3b55c49
Vanilla <osu_Vanilla@126.com>
2022-02-05 18:57:26 +0800

    feat[fish]: Enable direnv-nix, used in Projects/masm.

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -37,4 +37,8 @@
   # https://github.com/haslersn/any-nix-shell
   programs.fish.interactiveShellInit =
     "any-nix-shell fish --info-right | source";
+
+  # https://github.com/nix-community/nix-direnv
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
 }
