1b4f25be5786a3181c1a3d146de411c49ebd798f
Vanilla <osu_Vanilla@126.com>
2022-02-05 20:48:27 +0800

    style[programs]: Remove inputs.Vanilla from qv2ray.nix.

--- home-manager/programs/autostart/qv2ray.nix
+++ home-manager/programs/autostart/qv2ray.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, config, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = [
     pkgs.qv2ray
@@ -8,7 +8,12 @@
     })
   ];
 
-  imports = [ "${inputs.Vanilla}/prepareVCore.nix" ];
+  home.file.".config/qv2ray-vcore".source = with pkgs;
+    runCommand "vcore" { } ''mkdir $out/ && \
+      ln -s ${v2ray}/bin/v2ray $out/v2ray && ln -s ${v2ray}/bin/v2ctl $out/v2ctl
+      ln -s ${v2ray-geoip}/share/v2ray/geoip.dat $out/geoip.dat
+      ln -s ${v2ray-domain-list-community}/share/v2ray/geosite.dat $out/geosite.dat
+    '';
 
   home.file.".config/qv2ray".source =
     config.lib.file.mkOutOfStoreSymlink
