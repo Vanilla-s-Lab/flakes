820d6a581dc45e6a1da220e19bca0ee19c8472e3
Vanilla <osu_Vanilla@126.com>
2022-02-05 14:50:06 +0800

    feat[gnome]: Increase nautilus sidebar width.

--- home-manager/gnome/nautilus.nix
+++ home-manager/gnome/nautilus.nix
@@ -8,6 +8,8 @@
   dconf.settings = {
     # Nautilus - Scroll Down Twice.
     "org/gnome/nautilus/icon-view".default-zoom-level = "small";
+    # Nautilus - Let sidebar contains '[ip]:...' of ftp.
+    "org/gnome/nautilus/window-state".sidebar-width = 211;
   };
 
   home.file.".config/gtk-3.0/bookmarks".text = ''
