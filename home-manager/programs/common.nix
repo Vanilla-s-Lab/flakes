0c2353e1a6a22d40f50a76b0e985776ebf81edd6
Vanilla <osu_Vanilla@126.com>
2022-02-09 15:24:55 +0800

    perf[common]: Add dconf for virt-manager connection.

--- home-manager/programs/common.nix
+++ home-manager/programs/common.nix
@@ -3,4 +3,9 @@
   home.packages = [
     pkgs.virt-manager
   ];
+
+  dconf.settings."org/virt-manager/virt-manager/connections" = {
+    autoconnect = [ "qemu:///system" ];
+    uris = [ "qemu:///system" ];
+  };
 }
