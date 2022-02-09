2279780364480fad0ed7c8e4ca83a2d18b8c8627
Vanilla <osu_Vanilla@126.com>
2022-02-09 18:06:38 +0800

    style[gnome]: name > apps in folder.nix.

--- home-manager/gnome/folders.nix
+++ home-manager/gnome/folders.nix
@@ -8,26 +8,25 @@
     ];
 
     "org/gnome/desktop/app-folders/folders/d3165349-919a-496a-acbb-7a59027f0f54" = {
+      name = "Fcitx 5";
       apps = [
         "org.fcitx.Fcitx5.desktop"
         "fcitx5-configtool.desktop"
         "org.fcitx.fcitx5-migrator.desktop"
       ];
-
-      name = "Fcitx 5";
     };
 
     "org/gnome/desktop/app-folders/folders/4e0291de-0741-434f-94ef-75ff8dc5ef2c" = {
+      name = "WeChat";
       apps = [
         "wine-wechat.desktop"
         "wine-wechat-cfg.desktop"
         "wine-wechat-dir.desktop"
       ];
-
-      name = "WeChat";
     };
 
     "org/gnome/desktop/app-folders/folders/466c96ac-0106-4985-a59c-404a9d69a44e" = {
+      name = "WPS Office";
       apps = [
         "wps-office-prometheus.desktop"
         "wps-office-pdf.desktop"
@@ -35,8 +34,6 @@
         "wps-office-et.desktop"
         "wps-office-wps.desktop"
       ];
-
-      name = "WPS Office";
     };
   };
 }
