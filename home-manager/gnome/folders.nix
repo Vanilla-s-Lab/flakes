5e39a93a77ef626205a17e7ef66eec3bedbc2fd9
Vanilla <osu_Vanilla@126.com>
2022-02-10 11:40:34 +0800

    perf[gnome]: Make Terminal and Autostart folder.

--- home-manager/gnome/folders.nix
+++ home-manager/gnome/folders.nix
@@ -5,6 +5,8 @@
       "d3165349-919a-496a-acbb-7a59027f0f54" # Fcitx 5
       "4e0291de-0741-434f-94ef-75ff8dc5ef2c" # WeChat
       "466c96ac-0106-4985-a59c-404a9d69a44e" # WPS Office
+      "f31c95a8-8ed5-43d0-b849-c02b87cf6f2c" # Terminal
+      "046ad3d2-1480-422d-89a4-f8bd19324788" # Autostart
     ];
 
     "org/gnome/desktop/app-folders/folders/d3165349-919a-496a-acbb-7a59027f0f54" = {
@@ -34,6 +36,18 @@
         "wps-office-wpp.desktop"
         "wps-office-et.desktop"
         "wps-office-wps.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/f31c95a8-8ed5-43d0-b849-c02b87cf6f2c" =
+      { name = "Terminal"; apps = [ "Alacritty.desktop" "fish.desktop" ]; };
+
+    "org/gnome/desktop/app-folders/folders/046ad3d2-1480-422d-89a4-f8bd19324788" = {
+      name = "Autostart";
+      apps = [
+        "albert.desktop"
+        "kotatogramdesktop.desktop"
+        "qv2ray.desktop"
       ];
     };
   };
