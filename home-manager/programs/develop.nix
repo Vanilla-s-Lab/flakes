6847e68a49d00c7694518fce9d1e06e7e5767ce4
Vanilla <osu_Vanilla@126.com>
2022-02-10 15:39:56 +0800

    feat[develop]: Install PyCharm, with needed python packages.

--- home-manager/programs/develop.nix
+++ home-manager/programs/develop.nix
@@ -1,6 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = [
-    (pkgs.python3.withPackages (p: with p; [ pygobject3 ]))
+    (pkgs.python3.withPackages (p: with p; [ pygobject3 ]
+      ++ [ pip setuptools ] ++ [ mariadb XlsxWriter pandas ]))
+
+    pkgs.jetbrains.pycharm-professional
   ];
+
+  home.file.".cache/JetBrains".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot/cache/JetBrains";
+
+  home.file.".config/JetBrains".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot/config/JetBrains";
+
+  home.file.".local/share/JetBrains".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot/local/share/JetBrains";
+
+  home.file.".java/.userPrefs/jetbrains".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot/java/dot/userPrefs/jetbrains";
 }
