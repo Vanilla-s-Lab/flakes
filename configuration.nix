f73773c54ec05246a280257cea695b779eb0df4e
Vanilla <osu_Vanilla@126.com>
2022-02-12 22:04:21 +0800

    style[global]: Move network manager config -> ./configuration.nix.

--- configuration.nix
+++ configuration.nix
@@ -60,6 +60,14 @@
   # https://nixos.wiki/wiki/Dual_Booting_NixOS_and_Windows
   time.hardwareClockInLocalTime = true;
 
+  # Use NetworkManager, persist WiFi connections.
+  networking.networkmanager.enable = true;
+  environment.etc."NetworkManager/system-connections".source =
+    "/persistent/etc/NetworkManager/system-connections";
+
+  # intel/ibt-17-16-1.sfi | rtl_nic/rtl8411-2.fw
+  hardware.firmware = [ pkgsUnstable.linux-firmware ];
+
   # Configure keymap in X11
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
--- networking.nix
+++ networking.nix
@@ -4,14 +4,6 @@
   networking.proxy.default = "http://localhost:8889";
   networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Use NetworkManager, persist WiFi connections.
-  networking.networkmanager.enable = true;
-  environment.etc."NetworkManager/system-connections".source =
-    "/persistent/etc/NetworkManager/system-connections";
-
-  # intel/ibt-17-16-1.sfi | rtl_nic/rtl8411-2.fw
-  hardware.firmware = [ pkgsUnstable.linux-firmware ];
-
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 1089 8889 ];
   networking.firewall.allowedUDPPorts = [ 1089 8889 ];
