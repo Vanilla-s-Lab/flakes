fa626de11956e80c925e3fb5cfd13baef080ef3d
Vanilla <osu_Vanilla@126.com>
2022-02-12 11:38:46 +0800

    fix[nix]: Fix substituters and trusted-public-keys config alias.

--- nix.nix
+++ nix.nix
@@ -10,7 +10,7 @@
   programs.git.enable = true;
 
   # Liya-Fedin's cachix for building Kotatogram.
-  nix.settings.substituters = [
+  nix.binaryCaches = [
     "https://nixos-cn.cachix.org/"
     "https://ilya-fedin.cachix.org/"
     "https://kdna.cachix.org/"
@@ -18,7 +18,7 @@
   ];
 
   # https://github.com/cachix/install-nix-action
-  nix.settings.trusted-public-keys = [
+  nix.binaryCachePublicKeys = [
     "nixos-cn.cachix.org-1:L0jEaL6w7kwQOPlLoCR3ADx+E3Q8SEFEcB9Jaibl0Xg="
     "ilya-fedin.cachix.org-1:QveU24a5ePPMh82mAFSxLk1P+w97pRxqe9rh+MJqlag="
     "kdna.cachix.org-1:xAkLlelk3u0frMsevzcDLsjGe6ifqXx8q+OrPkbmetI="
