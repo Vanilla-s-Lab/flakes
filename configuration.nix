7fbbfda84df97aceda1b9a14e1dfe5fa7666b688
Vanilla <osu_Vanilla@126.com>
2022-02-12 22:17:22 +0800

    feat[global]: Add comment of libinputs, from commit d207647.

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,9 @@
   hardware.pulseaudio.extraModules = [ pkgs.pulseaudio-modules-bt ];
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
 
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
