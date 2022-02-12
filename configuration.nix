9f57f6d344bec25e1ce488bfacbe15311e48ed73
Vanilla <osu_Vanilla@126.com>
2022-02-12 22:05:24 +0800

    style[global]: Move networking proxy config -> ./configuration.nix.

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,10 @@
   # intel/ibt-17-16-1.sfi | rtl_nic/rtl8411-2.fw
   hardware.firmware = [ pkgsUnstable.linux-firmware ];
 
+  # Configure network proxy if necessary
+  networking.proxy.default = "http://localhost:8889";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
   # Configure keymap in X11
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
--- networking.nix
+++ networking.nix
@@ -1,9 +1,5 @@
 { pkgsUnstable, ... }:
 {
-  # Configure network proxy if necessary
-  networking.proxy.default = "http://localhost:8889";
-  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 1089 8889 ];
   networking.firewall.allowedUDPPorts = [ 1089 8889 ];
