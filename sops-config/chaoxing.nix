f1ba8f487ccb0a9b7f1b364d1f361f2ff21557c3
Vanilla <osu_Vanilla@126.com>
2022-02-24 11:06:07 +0800

    perf[secrets]: Set user permissions for chaoxing.yaml.

--- sops-config/chaoxing.nix
+++ sops-config/chaoxing.nix
@@ -1,5 +1,10 @@
 { config, ... }:
 {
   sops.secrets."chaoxing/username".sopsFile = ../secrets/chaoxing.yaml;
+  sops.secrets."chaoxing/username".owner = "vanilla";
+  sops.secrets."chaoxing/username".group = "users";
+
   sops.secrets."chaoxing/password".sopsFile = ../secrets/chaoxing.yaml;
+  sops.secrets."chaoxing/password".owner = "vanilla";
+  sops.secrets."chaoxing/password".group = "users";
 }
