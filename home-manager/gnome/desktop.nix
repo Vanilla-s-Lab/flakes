2d33843287bf4b6b33780ff016fc90a1e569e876
Vanilla <osu_Vanilla@126.com>
2022-03-11 19:34:56 +0800

    style[gnome]: Remove Terminal folder.

--- home-manager/gnome/desktop.nix
+++ home-manager/gnome/desktop.nix
@@ -14,7 +14,6 @@
       "d3165349-919a-496a-acbb-7a59027f0f54" # Fcitx 5
       "4e0291de-0741-434f-94ef-75ff8dc5ef2c" # Work
       "466c96ac-0106-4985-a59c-404a9d69a44e" # WPS Office
-      "f31c95a8-8ed5-43d0-b849-c02b87cf6f2c" # Terminal
       "046ad3d2-1480-422d-89a4-f8bd19324788" # Autostart
       "22647c69-a025-4142-ace6-9ea9a0cbdca1" # IDEs
     ];
@@ -49,9 +48,6 @@
         "wps-office-wps.desktop"
       ];
     };
-
-    "org/gnome/desktop/app-folders/folders/f31c95a8-8ed5-43d0-b849-c02b87cf6f2c" =
-      { name = "Terminal"; apps = [ "Alacritty.desktop" "fish.desktop" ]; };
 
     "org/gnome/desktop/app-folders/folders/046ad3d2-1480-422d-89a4-f8bd19324788" = {
       name = "Autostart";
