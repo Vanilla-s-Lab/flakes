a9bd7c6bd89dedfba679f90bb7a473abdee50b0d
Vanilla <osu_Vanilla@126.com>
2022-03-14 10:49:03 +0800

    style[file-linkes]: Remove persist of ~/.cache/nix/.

--- home-manager/file-links.nix
+++ home-manager/file-links.nix
@@ -1,9 +1,5 @@
 { config, ... }:
 {
-  home.file.".cache/nix".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "/persistent/dot/cache/nix";
-
   home.file."Projects".source =
     config.lib.file.mkOutOfStoreSymlink
       "/persistent/Projects";
