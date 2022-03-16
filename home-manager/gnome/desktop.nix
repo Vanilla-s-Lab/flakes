1b90f6e6ab28c0d63ef94ca62b322ce161883114
Vanilla <osu_Vanilla@126.com>
2022-03-16 19:50:16 +0800

    feat[GNOME]: Add Terminal folder, contains alacritty, fish and xterm.

--- home-manager/gnome/desktop.nix
+++ home-manager/gnome/desktop.nix
@@ -14,6 +14,7 @@
       "d3165349-919a-496a-acbb-7a59027f0f54" # Fcitx 5
       "4e0291de-0741-434f-94ef-75ff8dc5ef2c" # Work
       "466c96ac-0106-4985-a59c-404a9d69a44e" # WPS Office
+      "f31c95a8-8ed5-43d0-b849-c02b87cf6f2c" # Terminal
       "046ad3d2-1480-422d-89a4-f8bd19324788" # Autostart
       "22647c69-a025-4142-ace6-9ea9a0cbdca1" # IDEs
     ];
@@ -47,6 +48,11 @@
         "wps-office-et.desktop"
         "wps-office-wps.desktop"
       ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/f31c95a8-8ed5-43d0-b849-c02b87cf6f2c" = {
+      name = "Terminal";
+      apps = [ "Alacritty.desktop" "fish.desktop" "xterm.desktop" ];
     };
 
     "org/gnome/desktop/app-folders/folders/046ad3d2-1480-422d-89a4-f8bd19324788" = {
