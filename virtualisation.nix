04d0d4c08a1fddd40b005f82eacfff4757388f0b
Vanilla <osu_Vanilla@126.com>
2022-03-18 17:23:49 +0800

    feat[virtualisation]: Replace Docker with Podman, drop minikube.

--- virtualisation.nix
+++ virtualisation.nix
@@ -2,9 +2,7 @@
 {
   # https://nixos.wiki/wiki/Virt-manager
   virtualisation.libvirtd.enable = true;
-  users.users."vanilla".extraGroups = [ "libvirtd" ]
-    ++ [ "docker" ]; # GitHub - k3d-io/k3d - #84
-  virtualisation.docker.enable = true;
+  users.users."vanilla".extraGroups = [ "libvirtd" ];
 
   # The default KVM virtual storage location.
   environment.persistence."/persistent" = {
@@ -14,7 +12,7 @@
   # Support running binary of armv7l architecture.
   boot.binfmt.emulatedSystems = [ "armv7l-linux" ];
 
-  systemd.tmpfiles.rules = [
-    "L /var/lib/docker - - - - /persistent/var/lib/docker"
-  ];
+  # https://nixos.wiki/wiki/Podman
+  virtualisation.podman.enable = true;
+  virtualisation.podman.dockerCompat = true;
 }
