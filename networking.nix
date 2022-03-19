d5f762fa2a6560a239d65b07e462a005cc3de296
Vanilla <osu_Vanilla@126.com>
2022-03-19 11:39:25 +0800

    feat[networking]: Enable Wireshark, the GUI package.

--- networking.nix
+++ networking.nix
@@ -7,4 +7,8 @@
   # https://userbase.kde.org/KDEConnect#Troubleshooting
   networking.firewall.allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
   networking.firewall.allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
+
+  programs.wireshark.enable = true;
+  programs.wireshark.package = pkgs.wireshark;
+  users.users."vanilla".extraGroups = [ "wireshark" ];
 }
