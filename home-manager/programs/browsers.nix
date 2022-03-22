246724226362bebedb83bcb3407c73144b2f9866
Vanilla <osu_Vanilla@126.com>
2022-03-22 19:23:06 +0800

    feat[browser]: Install Tor Browser, persist the dir.

--- home-manager/programs/browsers.nix
+++ home-manager/programs/browsers.nix
@@ -1,12 +1,17 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   programs.chromium.enable = true;
+  home.packages = [ pkgs.tor-browser-bundle-bin ];
 
   # https://wiki.archlinux.org/title/chromium
   systemd.user.sessionVariables = {
     GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
     GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
   };
+
+  home.file.".local/share/tor-browser".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot/local/share/tor-browser";
 
   home.file.".config/chromium".source =
     config.lib.file.mkOutOfStoreSymlink
