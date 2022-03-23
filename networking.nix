f809f678f429c354b357fa2e4a25bae88ee794af
Vanilla <osu_Vanilla@126.com>
2022-03-23 15:52:54 +0800

    fix[networking]: Open 8889 http proxy port for cluster, only allow cluster IP.

--- networking.nix
+++ networking.nix
@@ -11,4 +11,11 @@
   programs.wireshark.enable = true;
   programs.wireshark.package = pkgs.wireshark;
   users.users."vanilla".extraGroups = [ "wireshark" ];
+
+  networking.firewall.allowPing = false;
+  # https://www.garron.me/en/bits/iptables-open-port-for-specific-ip.html
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -p tcp -s 192.168.49.2 --dport 8889 -j ACCEPT
+    iptables -A OUTPUT -p tcp -d 192.168.49.2 --dport 8889 -j ACCEPT
+  '';
 }
