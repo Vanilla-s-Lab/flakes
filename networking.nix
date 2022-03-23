e6bc9624b03da8c3bb7c6c762f3ea7578b73d1c2
Vanilla <osu_Vanilla@126.com>
2022-03-23 15:18:52 +0800

    style[networking]: Disable 1089 & 8889 port, listen but not open.

--- networking.nix
+++ networking.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 1089 8889 ];
-  networking.firewall.allowedUDPPorts = [ 1089 8889 ];
+  # networking.firewall.allowedTCPPorts = [ 1089 8889 ];
+  # networking.firewall.allowedUDPPorts = [ 1089 8889 ];
 
   # https://userbase.kde.org/KDEConnect#Troubleshooting
   networking.firewall.allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
