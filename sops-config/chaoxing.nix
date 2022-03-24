61d4a361f6a9156e4a6599abbc1d7927a12afdc6
Vanilla <osu_Vanilla@126.com>
2022-03-24 10:06:39 +0800

    feat[secrets]: Add chaoxing login akisamu.

--- sops-config/chaoxing.nix
+++ sops-config/chaoxing.nix
@@ -7,4 +7,12 @@
   sops.secrets."chaoxing/vanilla/password".sopsFile = ../secrets/chaoxing.yaml;
   sops.secrets."chaoxing/vanilla/password".owner = "vanilla";
   sops.secrets."chaoxing/vanilla/password".group = "users";
+
+  sops.secrets."chaoxing/akisamu/username".sopsFile = ../secrets/chaoxing.yaml;
+  sops.secrets."chaoxing/akisamu/username".owner = "vanilla";
+  sops.secrets."chaoxing/akisamu/username".group = "users";
+
+  sops.secrets."chaoxing/akisamu/password".sopsFile = ../secrets/chaoxing.yaml;
+  sops.secrets."chaoxing/akisamu/password".owner = "vanilla";
+  sops.secrets."chaoxing/akisamu/password".group = "users";
 }
