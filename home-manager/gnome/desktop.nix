f10873c8d82f058f01c192f0276cfc929f693f5a
Vanilla <osu_Vanilla@126.com>
2022-03-29 19:11:37 +0800

    fix[gnome]: nixos-unstable, remove AS version from desktop file.

--- home-manager/gnome/desktop.nix
+++ home-manager/gnome/desktop.nix
@@ -66,7 +66,7 @@
     "org/gnome/desktop/app-folders/folders/22647c69-a025-4142-ace6-9ea9a0cbdca1" = {
       name = "IDEs";
       apps = [
-        "android-studio-stable-2020.3.1.25.desktop"
+        "android-studio.desktop"
         "clion.desktop"
         "datagrip.desktop"
         "idea-ultimate.desktop"
