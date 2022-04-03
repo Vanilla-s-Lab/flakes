8207bd5fca46f41f49982e47912c2e2e57dc8224
Vanilla <osu_Vanilla@126.com>
2022-04-03 15:11:00 +0800

    feat[8080]: nodePackages.http-server -> python3 -m http-server.

--- configuration.nix
+++ configuration.nix
@@ -4,8 +4,8 @@
 
 { config, pkgs, inputs, lib, ... }:
 {
-  # networking.firewall.allowedTCPPorts = [ 8080 ];
-  # networking.firewall.allowedUDPPorts = [ 8080 ];
+  networking.firewall.allowedTCPPorts = [ 8000 ];
+  networking.firewall.allowedUDPPorts = [ 8000 ];
 
   programs.java.enable = true;
   programs.java.package = pkgs.jdk17_headless;
