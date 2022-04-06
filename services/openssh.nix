102fb1f0fe2c9eacf62171d889745614a0b9a460
Vanilla <osu_Vanilla@126.com>
2022-04-06 16:03:12 +0800

    feat[openssh]: Use hostKeys options instead of link.

--- services/openssh.nix
+++ services/openssh.nix
@@ -3,12 +3,15 @@
   services.openssh.enable = true;
   services.openssh.openFirewall = true;
 
-  environment.etc."ssh/ssh_host_ed25519_key".source =
-    "/persistent/etc/ssh/ssh_host_ed25519_key";
-  environment.etc."ssh/ssh_host_ed25519_key.pub".source =
-    "/persistent/etc/ssh/ssh_host_ed25519_key.pub";
-  environment.etc."ssh/ssh_host_rsa_key".source =
-    "/persistent/etc/ssh/ssh_host_rsa_key";
-  environment.etc."ssh/ssh_host_rsa_key.pub".source =
-    "/persistent/etc/ssh/ssh_host_rsa_key.pub";
+  services.openssh.hostKeys = [
+    {
+      bits = 4096;
+      path = "/persistent/etc/ssh/ssh_host_rsa_key";
+      type = "rsa";
+    }
+    {
+      path = "/persistent/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }
+  ];
 }
