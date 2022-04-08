1bfc7f1997af85375805e7470c55ba77def1333e
Vanilla <osu_Vanilla@126.com>
2022-04-08 17:30:17 +0800

    fix[dhcp4]: Using AliDNS for faster connection.

--- services/dhcpd4.nix
+++ services/dhcpd4.nix
@@ -7,7 +7,7 @@
   services.dhcpd4.extraConfig = ''
     subnet 10.0.10.0 netmask 255.255.255.0 {
             range 10.0.10.2 10.0.10.254;
-            option domain-name-servers 8.8.8.9,8.8.4.4;
+            option domain-name-servers 223.5.5.5,223.6.6.6;
             option routers 10.0.10.1;
             interface wlp0s20f3;
     }
