e7fa26fe17c5a960d8ed603c0bae3d56c06dc287
Vanilla <osu_Vanilla@126.com>
2022-04-08 11:14:55 +0800

    feat[hostapd]: Config a troll WiFI hostpot, WIP.

--- services/hostapd.nix
+++ services/hostapd.nix
@@ -2,4 +2,10 @@
 {
   services.hostapd.enable = true;
   services.hostapd.interface = "wlp0s20f3";
+
+  # https://en.wikipedia.org/wiki/ISO_3166-1
+  services.hostapd.countryCode = "CN";
+
+  services.hostapd.ssid = "Never Gonna Give You Up";
+  services.hostapd.wpa = false;
 }
