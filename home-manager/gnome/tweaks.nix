09253229bb1510f8a5368f29b86a002342f5053d
Vanilla <osu_Vanilla@126.com>
2022-04-08 22:12:41 +0800

    feat[flat-remix-gnome]: Test NixOS/nixpkgs/pull/167880 and works fine.

--- home-manager/gnome/tweaks.nix
+++ home-manager/gnome/tweaks.nix
@@ -6,7 +6,15 @@
     pkgs.layan-gtk-theme
     pkgs.breeze-gtk
     pkgs.tela-icon-theme
-    pkgs.flat-remix-gnome
+
+    # https://github.com/NixOS/nixpkgs/pull/167880
+    (pkgs.flat-remix-gnome.overrideAttrs (old: rec {
+      version = "20220407";
+      src = pkgs.fetchFromGitHub (
+        { owner = "daniruiz"; repo = "${old.pname}"; rev = "${version}"; } //
+        { hash = "sha256-Q54uF49NbvupN6LH80bgRtvyW7Cqm9vqsWXDkQrF4HQ="; }
+      );
+    }))
   ];
 
   dconf.settings = {
