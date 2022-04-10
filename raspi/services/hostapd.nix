010c18f00cd749688b662b4723174e7051694ff2
Vanilla <osu_Vanilla@126.com>
2022-04-10 19:38:49 +0800

    fix[raspi]: Enable ipv4 ip_forward, disable v6.

--- raspi/services/hostapd.nix
+++ raspi/services/hostapd.nix
@@ -8,4 +8,7 @@
 
   services.hostapd.ssid = "Never Gonna Give You Up";
   services.hostapd.wpa = false;
+
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."net.ipv6.conf.wlan0.disable_ipv6" = 1;
 }
