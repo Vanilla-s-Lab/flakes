646071573c5324a07b1a7b2f8dde061b6d88105c
Vanilla <osu_Vanilla@126.com>
2022-04-10 19:09:55 +0800

    feat[raspi]: Set hostname, nm -> eth0 DHCP.

--- raspi/configuration.nix
+++ raspi/configuration.nix
@@ -1,9 +1,10 @@
 { ... }:
 {
-  # TODO: Use NixOS options with interface name.
-  networking.networkmanager.enable = true;
+  # networking.networkmanager.enable = true;
+  networking.interfaces."eth0".useDHCP = true;
 
   services.openssh.enable = true;
+  networking.hostName = "NixOS-Raspi";
 
   services.fail2ban.enable = true;
   services.fail2ban.bantime-increment.enable = true;
