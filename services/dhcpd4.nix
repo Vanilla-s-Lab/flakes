a39f515393a2fb967c0b3c3b62964d89b4672b6c
Vanilla <osu_Vanilla@126.com>
2022-04-10 10:11:17 +0800

    fix[dhcpd4]: Enable routers as DNS server, use smartdns.

--- services/dhcpd4.nix
+++ services/dhcpd4.nix
@@ -7,7 +7,7 @@
   services.dhcpd4.extraConfig = ''
     subnet 10.0.10.0 netmask 255.255.255.0 {
             range 10.0.10.2 10.0.10.254;
-            option domain-name-servers 223.5.5.5,223.6.6.6;
+            option domain-name-servers 10.0.10.1;
             option routers 10.0.10.1;
             interface wlp0s20f3;
     }
