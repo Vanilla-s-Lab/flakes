fbd18f4029734e3806ec99e29be5e04fa18dab59
Vanilla <osu_Vanilla@126.com>
2022-04-11 13:46:42 +0800

    feat[rtl88x2bu]: Self packaged kernel driver for CF-812AC.

---
+++ home-manager/packages/rtl88x2bu/default.nix
@@ -0,0 +1,31 @@
+{ stdenv, kernel, fetchFromGitHub, bc, lib, ... }:
+stdenv.mkDerivation rec {
+  repo = "rtl88x2bu";
+  rev = "5.8.7.1_35809.20191129_COEX20191120-7777";
+
+  pname = "${repo}-${rev}-${kernel.version}";
+  version = "${rev}";
+
+  src = fetchFromGitHub {
+    owner = "cilynx";
+    repo = "${repo}";
+    rev = "${rev}";
+    hash = "sha256-urDG898qwb+1WmR/V2PPCxSw0ReXiaBpxhzCKmhxQ2I=";
+  };
+
+  # https://github.com/cilynx/rtl88x2bu/blob/5.8.7.1_35809.20191129_COEX20191120-7777/Makefile#L1355=
+  patches = [ ./nix_install.patch ./no-depmod.patch ]; # [1] related to [0]!
+  postPatch = "substituteInPlace Makefile --replace 'NIX_INSTALL' $out";
+
+  # https://nixos.wiki/wiki/Linux_kernel#Packaging_out-of-tree_kernel_modules
+  nativeBuildInputs = kernel.moduleBuildDependencies ++ [ bc ];
+
+  makeFlags = [
+    "KVER=${kernel.modDirVersion}"
+    "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+  ] ++ lib.singleton (" " + "-j 6" + " ");
+
+  # /lib/modules/5.15.16-lqx2/kernel/drivers/net/wireless/
+  MODDESTDIR = "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/";
+  installPhase = "mkdir -p ${MODDESTDIR} && make install";
+}
