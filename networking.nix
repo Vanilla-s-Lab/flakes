7cb5310e5f0b650c610744126960f49c48ad0479
Vanilla <osu_Vanilla@126.com>
2022-04-14 16:52:00 +0800

    fix[nm]: Also add wlp0s20f3 interface, no remember DHCP.

--- networking.nix
+++ networking.nix
@@ -19,12 +19,13 @@
   networking.firewall.checkReversePath = "loose";
 
   # https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/713#note_890189
+  # https://unix.stackexchange.com/questions/67898/using-the-not-equal-operator-for-string-comparison
   networking.networkmanager.dispatcherScripts = lib.singleton {
     source = pkgs.writeText "50-delete-lease" ''
       ifname="$1"
       event="$2"
 
-      [ "$ifname" != enp3s0f1 ] && exit 0
+      [ "$ifname" != enp3s0f1 ] && [ "$ifname" != wlp0s20f3 ] && exit 0
       [ "$event" != down ] && exit 0
 
       rm /var/lib/NetworkManager/*-"$CONNECTION_UUID"-"$ifname".lease
