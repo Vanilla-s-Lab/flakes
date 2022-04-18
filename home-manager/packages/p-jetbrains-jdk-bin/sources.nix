5204e5dcd9a462b923eea75476c59af17f8426a1
Vanilla <osu_Vanilla@126.com>
2022-04-18 20:13:49 +0800

    feat[p-jetbrains-jdk-bin]: From NixOS/nixpkgs/issues/160468.

---
+++ home-manager/packages/p-jetbrains-jdk-bin/sources.nix
@@ -0,0 +1,12 @@
+{
+  sources = {
+    p-jetbrains-jdk-bin = {
+      pname = "p-jetbrains-jdk-bin";
+      version = "17_0_2-linux-x64-b315.1";
+      src = fetchurl {
+        url = "https://cache-redirector.jetbrains.com/intellij-jbr/jbr_jcef-17_0_2-linux-x64-b315.1.tar.gz";
+        sha256 = "sha256-ImnQgErO3f7lNVHSSCKaPAD3RMsK2jmdA6VWX2WI2qA=";
+      };
+    };
+  };
+}
