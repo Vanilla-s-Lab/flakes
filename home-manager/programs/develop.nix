583d85bc06a09827cfac1da306eb06f3408de936
Vanilla <osu_Vanilla@126.com>
2022-04-24 19:33:46 +0800

    fix[develop.nix]: Disable pkgs.azure-cli, waiting upstream for fix.

--- home-manager/programs/develop.nix
+++ home-manager/programs/develop.nix
@@ -57,7 +57,8 @@ let pkgs_nodogsplash = pkgs.callPackage ../packages/nodogsplash.nix { }; in
     # pkgs.terraform-full
     pkgs.terraform
 
-    pkgs.azure-cli
+    # https://github.com/NixOS/nixpkgs/pull/169829
+    # pkgs.azure-cli
     pkgs.azure-storage-azcopy
 
     pkgs.gh
