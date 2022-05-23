23051c223270d5739cb5f6200285b262a0eb18b0
Vanilla <osu_Vanilla@126.com>
2022-05-23 21:38:48 +0800

    feat[fileSystems.nix]: sysctl set vm.swappiness to 0, no swap.

--- hardware/fileSystems.nix
+++ hardware/fileSystems.nix
@@ -5,6 +5,9 @@
     "/boot" = { device = "/dev/disk/by-uuid/2F71-8FE8"; fsType = "vfat"; };
   };
 
+  # https://www.howtogeek.com/449691/
+  boot.kernel.sysctl."vm.swappiness" = 0;
+
   fileSystems."/nix" = {
     device = "/dev/disk/by-uuid/49a55786-a043-400f-a6cb-3444ddda7975";
     fsType = "btrfs";
