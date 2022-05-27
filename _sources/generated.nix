48c00f339d0114910d17fa52d747618eb9abb6da
Vanilla <osu_Vanilla@126.com>
2022-05-27 17:01:06 +0800

    update[oh-my-posh]: v7.92.0 -> v7.92.1.

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -26,13 +26,13 @@
   };
   oh-my-posh = {
     pname = "oh-my-posh";
-    version = "v7.92.0";
+    version = "v7.92.1";
     src = fetchFromGitHub ({
       owner = "JanDeDobbeleer";
       repo = "oh-my-posh";
-      rev = "v7.92.0";
+      rev = "v7.92.1";
       fetchSubmodules = false;
-      sha256 = "sha256-GvPTTq2ljEoeVHnOuokRne2ffoz1ka7bE1SAB9wdZ7g=";
+      sha256 = "sha256-fc2mSONKlLbrEegYhT2o6evOQsoP7U4OZz3H8Brn5sM=";
     });
   };
   rose-pine = {
