183ab412d0bd73058da84c5c24f0ba81b2938725
Vanilla <osu_Vanilla@126.com>
2022-05-29 11:57:23 +0800

    style[terminal.nix]: Remove devShell of azure-nix.

--- home-manager/programs/terminal.nix
+++ home-manager/programs/terminal.nix
@@ -2,7 +2,6 @@
 let myh2o_backup = "098ed3541f61e477d9c9185fad15605b91644dcc34819ac700585abc27fba113"; in
 let Videos = "59682d0a8bc570a588c66fff6d2f6b03ef4afc5e9c2eac86ee770beafa61328f"; in
 let chaoxing_auto_sign = "187e3c25d7c87f426bee0324ad1ad5a285360a1a4947cbc9983147e3b9573880"; in
-let azure-nix = "1bd03d69e4884dcfa883f44694cf239592f6cb21ca25ea6c485f5dd550eaef9a"; in
 let Digital_Image_Processing = "fb8675f058d5c5216062b3f132802d6311e3bad75b78b9844e30a368faabd1e4"; in
 
 let enable_feature = feature_list: builtins.listToAttrs
@@ -145,8 +144,6 @@ let wxid = "wxid_2tafg8vy4onr22"; in
     "/persistent/Videos/.envrc";
   home.file.".local/share/direnv/allow/${chaoxing_auto_sign}".text =
     "/persistent/Documents/chaoxing_auto_sign/.envrc";
-  home.file.".local/share/direnv/allow/${azure-nix}".text =
-    "/persistent/Projects/azure-nix/.envrc";
   home.file.".local/share/direnv/allow/${Digital_Image_Processing}".text =
     "/persistent/Projects/Digital_Image_Processing/.envrc";
 
