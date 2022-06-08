20817167616115e60aee947a52656c2d359f9553
Vanilla <osu_Vanilla@126.com>
2022-06-08 12:31:30 +0800

    fix[steam.nix]: Add Win10 fonts for steam, fix gaming missing chinese.

--- steam.nix
+++ steam.nix
@@ -1,7 +1,15 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   programs.steam.enable = true;
-  # programs.steam.remotePlay.openFirewall = true;
+
+  nixpkgs.overlays = lib.singleton (self: super: {
+    steam = super.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        nur.repos.vanilla.Win10_LTSC_2021_fonts
+      ];
+    };
+  });
+
   boot.kernel.sysctl = {
     "dev.i915.perf_stream_paranoid" = 0;
   };
