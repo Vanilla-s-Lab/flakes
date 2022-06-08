6e533237b10323e4b042ff9221824592733f2db7
Vanilla <osu_Vanilla@126.com>
2022-06-08 19:06:23 +0800

    feat[configuration.nix]: Config for improve performance.

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,8 @@
   systemd.services."plymouth-quit".enable = false;
 
   # https://wiki.archlinux.org/title/improving_performance#Watchdogs
-  boot.kernelParams = [ "nowatchdog" ];
+  boot.kernelParams = [ "nowatchdog" ] # https://access.redhat.com/sites/default/files/attachments/201501-perf-brief-low-latency-tuning-rhel7-v2.1.pdf
+    ++ [ "mce=ignore_ce" "nosoftlockup" "audit=0" /* SELinux */ "idle=poll" /* isolcpus= */ "skew_tick=1" "intel_pstate=disable" /* nohz_full= */ ];
   boot.blacklistedKernelModules = [ "iTCO_wdt" ];
 
   imports = [
