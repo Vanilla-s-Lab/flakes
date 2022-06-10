916871ab24614d31eeccddfabb9c42e7545fb690
Vanilla <osu_Vanilla@126.com>
2022-06-10 19:18:53 +0800

    feat[vscode.nix]: Basic userSettings, for not sending messages to M$.

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -1,8 +1,22 @@
 { pkgs, ... }:
 {
   programs.vscode.enable = true;
-  programs.vscode.package = (pkgs.vscode-with-extensions.override {
+  programs.vscode.package = ((pkgs.vscode-with-extensions.override {
     vscodeExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace
       (builtins.fromJSON (builtins.readFile ../../vscode-extensions.json));
-  });
+  }).overrideAttrs (old: { pname = "vscode"; })); # Required by userSettings.
+
+  # https://code.visualstudio.com/docs/supporting/FAQ
+
+  programs.vscode.userSettings = {
+    "telemetry.telemetryLevel" = "off";
+    "workbench.enableExperiments" = false;
+
+    "update.mode" = "none";
+    "extensions.autoUpdate" = false;
+  };
+
+  home.file.".vscode/argv.json".text = builtins.toJSON {
+    "enable-crash-reporter" = false;
+  };
 }
