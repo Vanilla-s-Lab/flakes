5ec113c8dcacd9ec99d2e0a4f7a5f7b0034c5c6c
Vanilla <osu_Vanilla@126.com>
2022-06-12 16:54:42 +0800

    feat[common.nix]: Update disable WPS update minimal version.

--- home-manager/programs/common.nix
+++ home-manager/programs/common.nix
@@ -72,7 +72,7 @@
     common\system_check\no_necessary_symbol_fonts=false
 
     [UnixUpdateInfo]
-    UserRejectUpdateVersion=11.1.0.10976
+    UserRejectUpdateVersion=11.1.0.11664
   '';
 
   home.file.".local/share/Kingsoft/office6/skins/default/histroy.ini".text = ''
