9cfbacc5d15efb2b2e34aa6dd17b7de9607c68eb
Vanilla <osu_Vanilla@126.com>
2022-07-24 14:42:24 +0800

    feat[network.nix]: ip route add, but in Nix.

--- networking.nix
+++ networking.nix
@@ -9,4 +9,7 @@
 
   networking.networkmanager.wifi.macAddress = "permanent";
   networking.networkmanager.wifi.scanRandMacAddress = true;
+
+  networking.interfaces."wlp0s20f3".ipv4.routes = (lib.singleton
+    { address = "10.0.100.204"; prefixLength = 32; via = "192.168.5.1"; });
 }
