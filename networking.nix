ca95b257022b551c1110f95be17284e06cea48fa
Vanilla <osu_Vanilla@126.com>
2022-07-26 16:32:33 +0800

    feat[networking.nix]: Enable smokeping, monitoring Baidu & JumpServer.

--- networking.nix
+++ networking.nix
@@ -13,4 +13,21 @@
   networking.useNetworkd = true;
   networking.interfaces."wlp0s20f3".ipv4.routes = (lib.singleton
     { address = "10.0.100.204"; prefixLength = 32; via = "192.168.5.1"; });
+
+  services.smokeping.enable = true;
+  services.smokeping.targetConfig = ''
+    probe = FPing
+    menu = Default
+    title = Default
+
+    + TWX
+    menu = TWX
+    title = TWX
+
+    ++ Baidu
+    host = baidu.com
+
+    ++ JumpServer
+    host = 10.0.100.204
+  '';
 }
