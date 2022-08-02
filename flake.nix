eee7cf82dd299048758e2b319e4ae2c4f3b4ce2c
Vanilla <osu_Vanilla@126.com>
2022-08-02 09:26:17 +0800

    feat[flake.nix]: Turn inputs.nixpkgs-udev-fix to non flake.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     # https://github.com/NixOS/nixpkgs/commits/nixos-22.05/nixos/modules/services/hardware/udev.nix
     nixpkgs-udev-fix.url = "github:nixos/nixpkgs/89ace3967e0fefef19666366d64d315bae9b8f19";
+    nixpkgs-udev-fix.flake = false;
 
     # https://github.com/NixOS/nixpkgs/pull/133672/files
     nixpkgs-playwright-chromium.url = "github:jlesquembre/nixpkgs/playwright";
