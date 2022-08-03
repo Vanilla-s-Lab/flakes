180a563a92fe8661b66065270ae0a47f0fe602dd
Vanilla <osu_Vanilla@126.com>
2022-08-03 14:32:03 +0800

    feat[networking.nix]: Remove smokeping services.

--- networking.nix
+++ networking.nix
@@ -10,26 +10,6 @@
   networking.networkmanager.wifi.macAddress = "permanent";
   networking.networkmanager.wifi.scanRandMacAddress = true;
 
-  services.smokeping.enable = true;
-  services.smokeping.targetConfig = ''
-    probe = FPing
-    menu = Default
-    title = Default
-
-    + TWX
-    menu = TWX
-    title = TWX
-
-    ++ Baidu
-    host = baidu.com
-    ++ JumpServer
-    host = 10.0.100.204
-    ++ Sina
-    host = sina.com.cn
-    ++ Moka
-    host = mokahr.com
-  '';
-
   # services/x11/desktop-managers/gnome.nix
   services.avahi.enable = false; # [ 5353 ]
 }
