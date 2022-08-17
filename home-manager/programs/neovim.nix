417f7ff46fa5056a005def2af81dc1fc99c5e77e
Vanilla <osu_Vanilla@126.com>
2022-08-17 16:49:38 +0800

    fix[neovim.nix]: Install dashboard-vim plugin, solving 100% problem.

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -4,6 +4,11 @@ let generated = pkgs.callPackage ../../_sources/generated.nix { }; in
   programs.neovim.enable = true;
   programs.neovim.package = pkgs.neovim-nightly;
 
+  programs.neovim.plugins = with pkgs; [
+    # https://github.com/NvChad/NvChad/blob/v1.0/lua/pluginList.lua#L243
+    nur.repos.m15a.vimExtraPlugins.dashboard-nvim
+  ];
+
   programs.neovim.extraConfig = ''
     " https://vi.stackexchange.com/questions/6194/why-do-cw-and-ce-do-the-same-thing
     set cpoptions-=_
