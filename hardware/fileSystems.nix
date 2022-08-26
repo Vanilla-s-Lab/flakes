6b3aecac5ae1131b7387771fa0890749292a9951
Vanilla <osu_Vanilla@126.com>
2022-08-26 11:03:11 +0800

    feat[hardware/fileSystems.nix]: Remove swapfile, reconfig needed.

--- hardware/fileSystems.nix
+++ hardware/fileSystems.nix
@@ -5,9 +5,6 @@
     "/boot" = { device = "/dev/disk/by-uuid/2F71-8FE8"; fsType = "vfat"; };
   };
 
-  # https://itsfoss.com/create-swap-file-linux/
-  boot.kernel.sysctl."vm.swappiness" = 25;
-
   fileSystems."/nix" = {
     device = "/dev/disk/by-uuid/49a55786-a043-400f-a6cb-3444ddda7975";
     fsType = "btrfs";
@@ -36,17 +33,4 @@
 
   # https://github.com/NixOS/nixpkgs/pull/146497/files
   systemd.generators = { systemd-gpt-auto-generator = "/dev/null"; };
-
-  # https://opensource.com/article/19/2/swap-space-poll
-  swapDevices = lib.singleton {
-    device = "/swap/swapfile";
-    size = 1024 * 4; # 4 GB
-  };
-
-  # https://discourse.nixos.org/t/how-do-i-set-up-a-swap-file/8323/7
-  fileSystems."/swap" = {
-    device = "/dev/disk/by-uuid/49a55786-a043-400f-a6cb-3444ddda7975";
-    fsType = "btrfs";
-    options = [ "subvol=swap" "noatime" "compress-force=zstd" ];
-  };
 }
