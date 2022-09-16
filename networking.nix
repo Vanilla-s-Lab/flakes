385cfd4d907caa46ae5729ad17aa3a1abcbae1c2
Vanilla <osu_Vanilla@126.com>
2022-09-16 09:52:28 +0800

    feat[networking.nix]: Remove SMB service.

--- networking.nix
+++ networking.nix
@@ -1,25 +1,4 @@
 { pkgs, lib, config, ... }:
-let mkPublic = folders: (builtins.listToAttrs
-  (pkgs.lib.lists.forEach folders (x: {
-    name = "${x}";
-    value = {
-      public = "yes";
-      path = "/persistent/${x}";
-
-      # https://www.samba.org/samba/docs/current/man-html/smb.conf.5.html
-      "dfree command" = (pkgs.writeTextFile {
-        name = "junk";
-
-        text = ''
-          #! /bin/sh
-
-          echo "${builtins.toString (514 * 1024 * 1024)} ${builtins.toString (114 * 1024 * 1024)}"
-        '';
-
-        executable = true;
-      });
-    };
-  }))); in
 {
   programs.wireshark.enable = true;
   programs.wireshark.package = pkgs.wireshark;
@@ -34,17 +13,4 @@ let mkPublic = folders: (builtins.listToAttrs
   # services/x11/desktop-managers/gnome.nix
   services.avahi.enable = false; # [ 5353 ]
   services.geoclue2.enable = false; # Deps
-
-  services.samba.enable = true;
-  services.samba.openFirewall = true;
-
-  services.samba.shares = (mkPublic
-    [ "Music" "Videos" "Public" ]);
-
-  # https://github.com/Ninlives/emerge/blob/master/impl/lego/service/samba.nix
-  services.samba.extraConfig = "map to guest = Bad User";
-
-  services.samba-wsdd.enable = true;
-  networking.firewall.allowedTCPPorts = [ 5357 ];
-  networking.firewall.allowedUDPPorts = [ 3702 ];
 }
