e7aa69e352929521d339d4155d086f43a491c0df
Vanilla <osu_Vanilla@126.com>
2022-09-24 09:09:26 +0800

    feat[flake.nix]: Remove poetry2nix & neovim comment.

--- flake.nix
+++ flake.nix
@@ -30,12 +30,6 @@
     # https://nixos.wiki/wiki/Rust
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-
-    # https://github.com/nix-community/neovim-nightly-overlay
-    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-
-    poetry2nix.url = "github:nix-community/poetry2nix";
-    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, ... }@inputs: with inputs;
@@ -46,8 +40,6 @@
       nixosConfig = let overlays = [
         nur.overlay
         rust-overlay.overlays.default
-        # neovim-nightly-overlay.overlay
-        poetry2nix.overlay
       ]; in
         nixpkgs.lib.nixosSystem rec {
           inherit system; specialArgs = { inherit inputs self system pkgsUnstable; };
