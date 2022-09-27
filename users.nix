ef650d318edb5ca0bafa23ab16bf2b720c773bf9
Vanilla <osu_Vanilla@126.com>
2022-09-27 18:00:30 +0800

    feat[users.nix]: Remove global fish.

--- users.nix
+++ users.nix
@@ -4,9 +4,6 @@ let hashedPassword = "$6$NixOS/RoT$eo8dkH7eCxYFh/YkSOzDp7lIG1iQZm5MY2L5IPRh/1YIW
   # https://elis.nu/blog/2020/05/nixos-tmpfs-as-root/
   users.mutableUsers = false;
 
-  # https://github.com/fish-shell/fish-shell
-  programs.fish.enable = true;
-
   programs.adb.enable = true;
   users.users."vanilla" = {
     isNormalUser = true;
