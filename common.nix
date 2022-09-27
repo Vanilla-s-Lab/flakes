1c47ca4252a76f3c575c85b615544c8a9f0b4ecc
Vanilla <osu_Vanilla@126.com>
2022-09-27 17:38:43 +0800

    feat[common.nix]: Avoiding dmesg issues of 'windows_names' mount options.

--- common.nix
+++ common.nix
@@ -3,4 +3,12 @@
   # https://redis.io/docs/manual/admin/
   boot.kernel.sysctl = { "vm.overcommit_memory" = 1; };
   boot.kernelParams = [ "transparent_hugepage=never" ];
+
+  # https://wiki.archlinux.org/title/NTFS#udisks_support
+  services.udisks2.enable = true;
+  services.udisks2.settings."mount_options.conf" = {
+    defaults = {
+      ntfs_defaults = "uid=$UID,gid=$GID,noatime,prealloc";
+    };
+  };
 }
