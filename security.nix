6d970ac05fa3374eb422b98775d9f25b9e92eb00
Vanilla <osu_Vanilla@126.com>
2022-10-06 14:53:26 +0800

    chore[security.nix]: timestamp_timeout = 0 but commented.

--- security.nix
+++ security.nix
@@ -1,8 +1,13 @@
 { lib, pkgs, ... }:
 {
   security.sudo.enable = true;
+
   # https://superuser.com/questions/500119
-  security.sudo.extraConfig = "Defaults lecture = never";
+  # https://www.sudo.ws/docs/man/1.8.15/sudoers.man/
+  security.sudo.extraConfig = ''
+    Defaults lecture = never
+    # Defaults timestamp_timeout = 0
+  '';
 
   # https://nixos.wiki/wiki/Yubikey
   security.pam.yubico = {
