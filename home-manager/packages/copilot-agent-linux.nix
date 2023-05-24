d5125075686673cf22d1ea4005ee341cfd0a00c7
Vanilla <osu_Vanilla@126.com>
2023-05-24 17:26:57 +0800

    update[packages/copilot-agent-linux.nix]: 1.2.5.2507 -> 1.2.6.2594.

--- home-manager/packages/copilot-agent-linux.nix
+++ home-manager/packages/copilot-agent-linux.nix
@@ -1,12 +1,12 @@
 { buildFHSUserEnv, fetchurl, runCommand, unzip, ... }:
 
 let pname = "github-copilot-intellij"; in
-let version = "1.2.5.2507"; in
+let version = "1.2.6.2594"; in
 
 let
   src = fetchurl {
-    url = "https://plugins.jetbrains.com/files/17718/313933/${pname}-${version}.zip";
-    hash = "sha256-yWFe9Qjf7BZILtBHLiyTvo3wY/qtdRHU+5iJ6PqU518=";
+    url = "https://plugins.jetbrains.com/files/17718/331908/${pname}-${version}.zip";
+    hash = "sha256-Oe2Gh/KvdRzGaCm3ys4G/98rI5bYKQKYHngOfwn18Pk=";
   };
 in
 
