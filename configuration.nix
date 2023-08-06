8c7c1e3d06cbb4cb94af323034cf8bb5c44a784f
Vanilla <osu_Vanilla@126.com>
2023-08-06 21:04:32 +0800

    update[configuration.nix]: openssl-1.1.1u -> v.

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@ with inputs;
 {
   # https://github.com/NixOS/nixpkgs/pull/234359
   nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.1.1u"
+    "openssl-1.1.1v"
   ];
 
   programs.command-not-found.enable = true;
