6e3239f1fda4cc8c1632754bf1908b61fabf506f
Vanilla <osu_Vanilla@126.com>
2023-08-08 09:40:09 +0800

    clean[common.nix]: Remove kernel.sysctl & params.

--- common.nix
+++ common.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }:
 {
-  # https://redis.io/docs/manual/admin/
-  boot.kernel.sysctl = { "vm.overcommit_memory" = 1; };
-  boot.kernelParams = [ "transparent_hugepage=never" ];
-
   # https://wiki.archlinux.org/title/NTFS#udisks_support
   services.udisks2.enable = true;
   services.udisks2.settings."mount_options.conf" = {
