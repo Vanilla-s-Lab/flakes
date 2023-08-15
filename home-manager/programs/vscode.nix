9bae6ae98418d0344154ff37491276bd2eb1076a
Vanilla <osu_Vanilla@126.com>
2023-08-15 14:21:11 +0800

    feat[home-manager/programs/vscode.nix]: Config for Adwaita theme.

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -1,5 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, nix-vscode-extensions, system, ... }:
 {
   programs.vscode.enable = true;
   programs.vscode.package = pkgs.vscodium;
+
+  programs.vscode.extensions = with nix-vscode-extensions; [
+    extensions."${system}".vscode-marketplace.piousdeer.adwaita-theme
+  ];
+
+  # https://github.com/piousdeer/vscode-adwaita#suggested-settings
+  programs.vscode.userSettings = {
+    "window.titleBarStyle" = "custom";
+    "window.commandCenter" = true;
+    "window.autoDetectColorScheme" = true;
+    "workbench.preferredDarkColorTheme" = "Adwaita Dark";
+    "workbench.preferredLightColorTheme" = "Adwaita Light";
+    "workbench.productIconTheme" = "adwaita";
+    "editor.renderLineHighlight" = "none";
+    "workbench.iconTheme" = null;
+    "workbench.tree.indent" = 12;
+  };
 }
