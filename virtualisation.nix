f82ae7421dd13f6b906026af6d8be9d6b1f348ff
Vanilla <osu_Vanilla@126.com>
2023-11-14 14:51:40 +0800

    chore[virtualisation.nix]: Remove docker & podman.

--- virtualisation.nix
+++ virtualisation.nix
@@ -1,30 +1,6 @@
 { pkgs, inputs, lib, ... }:
 {
-  # https://nixos.wiki/wiki/Podman
-  users.users."vanilla".extraGroups =
-    [ "podman" /* "docker" */ ];
-
-  # https://nixos.wiki/wiki/Docker
-  # virtualisation.docker.enable = true;
-
-  # https://docs.docker.com/build/buildkit/
-  virtualisation.docker.daemon.settings = {
-    "features" = { "buildkit" = true; };
-  };
-
-  systemd.tmpfiles.rules = [
-    "L /var/lib/docker - - - - /persistent/var/lib/docker"
-  ];
-
   # Support running binary of armv7l architecture.
   boot.binfmt.emulatedSystems = [ "armv7l-linux" "aarch64-linux" ]
     ++ [ "riscv32-linux" "riscv64-linux" ];
-
-  # https://nixos.wiki/wiki/Podman
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerSocket.enable = true;
-
-  environment.systemPackages = [
-    (pkgs.callPackage ./home-manager/packages/docker-lock.nix { })
-  ];
 }
