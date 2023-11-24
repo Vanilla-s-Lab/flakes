7916c25d174aa2d312eff418b7b5c441f9f47dcd
Vanilla <osu_Vanilla@126.com>
2023-11-24 23:20:16 +0800

    feat[services/openssh.nix]: Enable with using yubikeys.

--- services/openssh.nix
+++ services/openssh.nix
@@ -1,10 +1,6 @@
 { pkgs, lib, ... }:
 {
   services.openssh.enable = true;
-  services.openssh.openFirewall = false;
-
-  services.openssh.listenAddresses =
-    lib.singleton { addr = "127.0.0.1"; port = 22; };
 
   services.openssh.hostKeys = [
     {
