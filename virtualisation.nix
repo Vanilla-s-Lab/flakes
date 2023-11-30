3dc11cbf0619cb543536a35f2c4089396824787b
Vanilla <osu_Vanilla@126.com>
2023-11-30 10:06:54 +0800

    feat[virtualisation.nix]: Enable Docker.

--- virtualisation.nix
+++ virtualisation.nix
@@ -1,5 +1,9 @@
 { pkgs, inputs, lib, ... }:
 {
+  # https://nixos.wiki/wiki/Docker
+  virtualisation.docker.enable = true;
+  users.users."vanilla".extraGroups = [ "docker" ];
+
   # Support running binary of armv7l architecture.
   boot.binfmt.emulatedSystems = [ "armv7l-linux" "aarch64-linux" ]
     ++ [ "riscv32-linux" "riscv64-linux" ];
