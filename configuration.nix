e41df4a0b8f47cabc40a5474ff7b89e86905fc5d
Vanilla <osu_Vanilla@126.com>
2024-02-18 10:19:27 +0800

    clean[services/nfs.nix]: Removed.

--- configuration.nix
+++ configuration.nix
@@ -35,9 +35,6 @@ with inputs;
     ./services/btrfs.nix
     ./services/smartdns.nix
 
-    # https://nixos.wiki/wiki/NFS
-    # ./services/nfs.nix
-
     ./sops-config/ArchiSteamFarm.nix
     ./sops-config/chaoxing.nix
 
