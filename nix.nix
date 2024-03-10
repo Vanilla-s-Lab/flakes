b539df80acca5042d9d0491924f06665b845b191
Vanilla <osu_Vanilla@126.com>
2024-03-10 20:54:55 +0800

    chore[nix.nix]: Remove nixos-cn substituters.

--- nix.nix
+++ nix.nix
@@ -13,13 +13,11 @@
   # https://mirrors.bfsu.edu.cn/help/nix/
   nix.settings.substituters = [
     "https://mirrors.bfsu.edu.cn/nix-channels/store"
-    "https://nixos-cn.cachix.org/"
   ];
 
   # https://github.com/cachix/install-nix-action
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "nixos-cn.cachix.org-1:L0jEaL6w7kwQOPlLoCR3ADx+E3Q8SEFEcB9Jaibl0Xg="
   ];
 
   # Used for impure `<nixpkgs>`.
